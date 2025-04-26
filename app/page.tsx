66607828f90a6ad43c843dcb621a2b43db118f71
originalbyteme <noahrijkaard@gmail.com>
2025-04-26 20:52:55 +0800

    Add gradient background with blobs + theme switch

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 import type { Metadata } from "next";
-import { Counter } from "./components/counter/Counter";
 
 import { SearchForm } from "@/app/components/search-form"
-// import { SpotifyLogo } from "@/app/components/spotify-logo"
+import LavaLampBackground from "@/app/components/ui/lavaLampBackground";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-zinc-900 to-black text-white">
-      <div className="container mx-auto px-4 py-8">
+    <div className="relative min-h-screen text-white overflow-hidden">
+      <LavaLampBackground />
+      <div className="relative container mx-auto px-4 py-8">
         <div className="flex flex-col items-center justify-center py-12">
           
-          <h1 className="text-4xl font-bold mb-6 text-center">Spotify Song Search</h1>
-          <p className="text-zinc-400 mb-8 text-center max-w-2xl">
+          <h1 className="text-4xl font-bold mb-6 text-center dark:text-white">Moodify</h1>
+          <p className="text-zinc-400 mb-8 text-center max-w-2xl dark:text-zinc-300">
             Search for your favorite songs, artists, and albums. Results update in real-time as you type.
           </p>
           <SearchForm />
