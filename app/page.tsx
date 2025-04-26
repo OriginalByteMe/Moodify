07c0af6c75c6de53d671bfc101c882460c26b8d9
originalbyteme <noahrijkaard@gmail.com>
2025-04-26 20:53:08 +0800

    change light mode text colours

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,13 @@ import LavaLampBackground from "@/app/components/ui/lavaLampBackground";
 
 export default function Home() {
   return (
-    <div className="relative min-h-screen text-white overflow-hidden">
+    <div className="relative min-h-screen text-black overflow-hidden">
       <LavaLampBackground />
       <div className="relative container mx-auto px-4 py-8">
         <div className="flex flex-col items-center justify-center py-12">
           
           <h1 className="text-4xl font-bold mb-6 text-center dark:text-white">Moodify</h1>
-          <p className="text-zinc-400 mb-8 text-center max-w-2xl dark:text-zinc-300">
+          <p className="text-zinc-900 mb-8 text-center max-w-2xl dark:text-zinc-300">
             Search for your favorite songs, artists, and albums. Results update in real-time as you type.
           </p>
           <SearchForm />
