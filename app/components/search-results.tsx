b957ef7b1260d9dba406560a5cf4790b06067d7e
originalbyteme <noahrijkaard@gmail.com>
2025-04-28 16:44:07 +0800

    Add functionality to change background on click of song

--- app/components/search-results.tsx
+++ app/components/search-results.tsx
@@ -4,7 +4,6 @@ import { SpotifyTrack } from "@/app/utils/interfaces"
 
 export function SearchResults() {
   const results = useSelector((state: { spotify: { tracks: SpotifyTrack[] } }) => state.spotify.tracks);
-  console.log('results:', results);
   if (!results || results.length === 0) {
     return <div className="mt-8 text-center text-zinc-400">No results found. Try a different search term.</div>
   }
